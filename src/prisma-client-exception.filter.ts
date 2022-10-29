753fff98b3795e334d35f63f860adf58055b417f
Collins Oriakhi <collins.oriakhi@itara.co>
2022-10-29 19:17:36 +0100

    Added Error Handling Basics

---
+++ src/prisma-client-exception.filter.ts
@@ -0,0 +1,33 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { Prisma } from '@prisma/client';
+import { Response } from 'express';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    switch (exception.code) {
+      case 'P2002':
+        const status = HttpStatus.CONFLICT;
+        const message = exception.message.replace(/\n/g, '');
+        response.status(status).json({
+          statusCode: status,
+          message,
+        });
+        break;
+      // TODO catch other error codes (e.g. 'P2000' or 'P2025')
+      default:
+        // TODO: 500 status code response
+        super.catch(exception, host);
+        break;
+    }
+  }
+}
+// nest generate filter prisma-client-exception
